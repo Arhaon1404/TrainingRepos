bffc6669fc453cc3bfe609d638c588880f2c7c6d
Arhaon1404 <niikholm@gmail.com>
2021-09-25 16:39:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Light_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
